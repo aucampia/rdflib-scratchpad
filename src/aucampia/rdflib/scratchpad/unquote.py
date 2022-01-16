4df349ba5f9d4de666f3a16afcd28b3e4e7976c9
Iwan Aucamp <aucampia@gmail.com>
2022-01-16 18:53:53 +0100

    wip

--- src/aucampia/rdflib/scratchpad/unquote.py
+++ src/aucampia/rdflib/scratchpad/unquote.py
@@ -1,5 +1,5 @@
 import re
-from typing import Match
+from typing import Callable, Dict, Match
 
 r_unicodeEscape = re.compile(r"(\\u[0-9A-Fa-f]{4}|\\U[0-9A-Fa-f]{8})")
 
@@ -22,44 +22,71 @@ _turtle_escape_pattern = re.compile(
     # re.ASCII,
 )
 
+smatch_variant: Dict[int, Callable[[str], str]] = {
+    1: lambda smatch: smatch.translate(_string_escape_translator),
+    2: lambda smatch: _string_escape_map[smatch],
+}
 
-def _turtle_escape_subber_dict(match: Match[str]) -> str:
+
+def _turtle_escape_subber(match: Match[str], variant: int) -> str:
     smatch, umatch = match.groups()
     if smatch is not None:
-        return _string_escape_map[smatch]
+        return smatch_variant[variant](smatch)
+        # if variant == 1:
+        #     return smatch.translate(_string_escape_translator)
+        # if variant == 2:
+        #     return _string_escape_map[smatch]
     else:
         return chr(int(umatch[1:], 16))
 
 
-def decodeUnicodeEscapeNewDict(escaped: str) -> str:
+def decodeUnicodeEscapeNew(escaped: str, variant: int) -> str:
     if "\\" not in escaped:
         # Most of times, there are no backslashes in strings.
         # In the general case, it could use maketrans and translate.
         return escaped
-    return _turtle_escape_pattern.sub(_turtle_escape_subber_dict, escaped)
+    return _turtle_escape_pattern.sub(
+        lambda match: _turtle_escape_subber(match, variant), escaped
+    )
 
 
-def _turtle_escape_subber(match: Match[str]) -> str:
-    smatch, umatch = match.groups()
-    if smatch is not None:
-        return smatch.translate(_string_escape_translator)
-    else:
-        return chr(int(umatch[1:], 16))
+# def _turtle_escape_subber_dict(match: Match[str]) -> str:
+#     smatch, umatch = match.groups()
+#     if smatch is not None:
+#         return _string_escape_map[smatch]
+#     else:
+#         return chr(int(umatch[1:], 16))
 
 
-def decodeUnicodeEscapeNew(escaped: str) -> str:
-    if "\\" not in escaped:
-        # Most of times, there are no backslashes in strings.
-        # In the general case, it could use maketrans and translate.
-        return escaped
-    return _turtle_escape_pattern.sub(_turtle_escape_subber, escaped)
+# def decodeUnicodeEscapeNewDict(escaped: str) -> str:
+#     if "\\" not in escaped:
+#         # Most of times, there are no backslashes in strings.
+#         # In the general case, it could use maketrans and translate.
+#         return escaped
+#     return _turtle_escape_pattern.sub(_turtle_escape_subber_dict, escaped)
+
+
+# def _turtle_escape_subber(match: Match[str]) -> str:
+#     smatch, umatch = match.groups()
+#     if smatch is not None:
+#         return smatch.translate(_string_escape_translator)
+#     else:
+#         return chr(int(umatch[1:], 16))
+
+
+# def decodeUnicodeEscapeNew(escaped: str) -> str:
+#     if "\\" not in escaped:
+#         # Most of times, there are no backslashes in strings.
+#         # In the general case, it could use maketrans and translate.
+#         return escaped
+#     return _turtle_escape_pattern.sub(_turtle_escape_subber, escaped)
 
 
 def _unicodeExpand(s: str) -> str:
     return r_unicodeEscape.sub(lambda m: chr(int(m.group(0)[2:], 16)), s)
 
 
-def decodeUnicodeEscape(s: str) -> str:
+def decodeUnicodeEscape(s: str, variant: int) -> str:
     """
     s is a unicode string
     replace ``\\n`` and ``\\u00AC`` unicode escapes
@@ -93,7 +120,7 @@ r_uniquot = re.compile(r"\\u([0-9A-Fa-f]{4})|\\U([0-9A-Fa-f]{8})")
 variants = {
     0: decodeUnicodeEscape,
     1: decodeUnicodeEscapeNew,
-    2: decodeUnicodeEscapeNewDict,
+    2: decodeUnicodeEscapeNew,
 }
 
 
@@ -111,7 +138,7 @@ def unquote(s: str, validate: bool = False, variant: int = 0) -> str:
         #     return decodeUnicodeEscapeNewDict(s)
         # else:
 
-        return variants[variant](s)
+        return variants[variant](s, variant)
     else:
         result = []
         while s:
